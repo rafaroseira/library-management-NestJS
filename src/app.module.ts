0b93acfa64af6b6406a1a1d42908bb361b524911
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-24 12:24:51 -0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { StudentModule } from './student/student.module';
 import { LoanModule } from './loan/loan.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { database } from './config/databaseConfig';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EmployeeModule, BookModule, StudentModule, LoanModule, TypeOrmModule.forRoot(database)],
+  imports: [EmployeeModule, BookModule, StudentModule, LoanModule, TypeOrmModule.forRoot(database), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
