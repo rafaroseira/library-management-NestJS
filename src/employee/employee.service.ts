cbdf421cad0a371694cbdacb87de2bd77be6c2ec
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-24 15:37:06 -0300

    hash password in create method

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -3,18 +3,26 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from './employee.entity';
 import { Repository } from 'typeorm';
 import { CreateEmployeeDTO } from './dto/create-employee.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class EmployeeService {
+
+    private readonly SALT_ROUNDS = 12;
 
     constructor(@InjectRepository(Employee) private employeeRepository: Repository<Employee>){}
 
     async create(createEmployeeDTO: CreateEmployeeDTO) {
 
+        const hashedPassword = await bcrypt.hash(
+            createEmployeeDTO.password, 
+            this.SALT_ROUNDS
+          );
+
         const employee = new Employee(
             createEmployeeDTO.name,
             createEmployeeDTO.email,
-            createEmployeeDTO.password
+            hashedPassword
         );
         
         await this.employeeRepository.save(employee);
