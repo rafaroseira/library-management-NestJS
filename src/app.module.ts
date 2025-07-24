ad5e500487bab01aecaca8ab9c0593a97a84bd20
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-24 09:14:09 -0300

    update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeModule } from './employee/employee.module';
+import { BookModule } from './book/book.module';
+import { StudentModule } from './student/student.module';
+import { LoanModule } from './loan/loan.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { database } from './config/databaseConfig';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule, BookModule, StudentModule, LoanModule, TypeOrmModule.forRoot(database)],
   controllers: [AppController],
   providers: [AppService],
 })
