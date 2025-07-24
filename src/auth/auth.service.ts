21ce6ff168c4741fe6754cbd2bf548747a03669e
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-24 15:16:30 -0300

    update auth service to support jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from 'src/employee/employee.entity';
 import { Repository } from 'typeorm';
@@ -6,13 +7,12 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectRepository(Employee) private employeeRepository: Repository<Employee>){}
+    constructor(@InjectRepository(Employee) private employeeRepository: Repository<Employee>,
+                                            private jwtService: JwtService){}
 
     async validateEmployee(email: string, password: string){
 
-        const employee = await this.employeeRepository.findOne({
-            where: { email }
-        });
+        const employee = await this.employeeRepository.findOneBy({email});
 
         if(employee && employee.password === password){
             const { password, ...rest } = employee;
@@ -21,4 +21,11 @@ export class AuthService {
 
         return null;
     }
+
+    async login(employee: any) {
+        const payload = { email: employee.email, sub: employee.id };
+        return {
+          access_token: this.jwtService.sign(payload),
+        };
+      }
 }
