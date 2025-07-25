7ceb64c169db44ffc3fec2435532dea530a1dd3f
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-25 15:02:40 -0300

    add guard to creation route

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, HttpStatus, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Request, UseGuards, ValidationPipe } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { CreateStudentDTO } from './dto/create-student.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('student')
 export class StudentController {
 
     constructor(private readonly studentService: StudentService){}
 
+    @UseGuards(JwtAuthGuard)
     @Post("/new")
-    async create(@Body(ValidationPipe) createStudentDTO: CreateStudentDTO){
-        await this.studentService.create(createStudentDTO);
+    async create(@Body(ValidationPipe) createStudentDTO: CreateStudentDTO, @Request() req){
+        await this.studentService.create(createStudentDTO, req.user.userId);
         
         return {
             statusCode: HttpStatus.CREATED,
