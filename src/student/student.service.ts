b56081f979ce2471ac3868cabd2d0f36bdafbf8d
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-25 15:01:53 -0300

    update create function

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,22 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Student } from './student.entity';
 import { Repository } from 'typeorm';
 import { CreateStudentDTO } from './dto/create-student.dto';
+import { Employee } from 'src/employee/employee.entity';
 
 @Injectable()
 export class StudentService {
 
-    constructor(@InjectRepository(Student) private StudentRepository: Repository<Student>){}
+    constructor(@InjectRepository(Student) private studentRepository: Repository<Student>,
+                @InjectRepository(Employee) private employeeRepository: Repository<Employee>){}
 
-    async create(createStudentDTO: CreateStudentDTO){
+    async create(createStudentDTO: CreateStudentDTO, employeeId: number){
+
+        if(await this.studentRepository.existsBy({ RA: createStudentDTO.RA })) throw new BadRequestException("Student already exists");
+
+        const employee = await this.employeeRepository.findOneBy({id: employeeId});
+
+        if(!employee) throw new NotFoundException("Employee not found");
         
         const student = new Student(
             createStudentDTO.name,
             createStudentDTO.RA,
-            createStudentDTO.phone
+            createStudentDTO.phone,
+            employee
         );
 
-        await this.StudentRepository.save(student);
+        await this.studentRepository.save(student);
     }
 }
