4a24bcad5d74e122592a9f06e94ce8f701cc346d
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-25 15:01:28 -0300

    update student module

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -3,9 +3,10 @@ import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Student } from './student.entity';
+import { Employee } from 'src/employee/employee.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Student])],
+  imports: [TypeOrmModule.forFeature([Student, Employee])],
   providers: [StudentService],
   controllers: [StudentController]
 })
