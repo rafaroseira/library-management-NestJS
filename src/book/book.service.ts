a6b65acc73ba9af3877770cf605089d33d92c2d6
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-28 12:10:56 -0300

    create update book details method

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { Like, Repository } from 'typeorm';
 import { CreateBookDTO } from './dto/create-book.dto';
 import { Employee } from 'src/employee/employee.entity';
+import { UpdateBookDTO } from './dto/update-book.dto';
 
 @Injectable()
 export class BookService {
@@ -41,6 +42,40 @@ export class BookService {
     async findOne(id: number){
         const book = await this.bookRepository.findOneBy({id});
         return book;
+    }
+
+    async update(updateBookDTO: UpdateBookDTO, id: number){
+
+        let book = await this.bookRepository.findOneBy({id});
+
+        if(!book) throw new NotFoundException("Book not found");
+
+        if(updateBookDTO.title !== undefined) book.title = updateBookDTO.title;
+        if(updateBookDTO.author !== undefined) book.author = updateBookDTO.author;
+
+        if(updateBookDTO.totalCopies !== undefined && updateBookDTO.totalCopies !== book.totalCopies){
+
+            let currentTotalCopies: number = book.totalCopies;
+            let newTotalCopies: number = updateBookDTO.totalCopies;
+
+            if(newTotalCopies > currentTotalCopies){
+
+                book.availableCopies += (newTotalCopies - currentTotalCopies);
+                
+            } else {
+
+                let copiesLoaned:number = book.totalCopies - book.availableCopies;
+                if(!(newTotalCopies >= copiesLoaned)) throw new BadRequestException("Not possible to update total copies while number of loaned copies is greater than the new total copies");
+
+                book.availableCopies -= (Math.abs(newTotalCopies - currentTotalCopies));
+                if(book.availableCopies < 0) book.availableCopies = 0;            
+            
+            }
+
+            book.totalCopies = newTotalCopies;
+        }
+
+        await this.bookRepository.save(book);
     }
 
 }
