006eb7d9ecfdbc62349c5361416ab109c619e7f6
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-08-15 10:50:33 -0300

    import ConfigModule and add joi for env variables validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,27 @@ import { LoanModule } from './loan/loan.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { database } from './config/databaseConfig';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [EmployeeModule, BookModule, StudentModule, LoanModule, TypeOrmModule.forRoot(database), AuthModule],
+  imports: [
+    EmployeeModule,
+    BookModule, 
+    StudentModule, 
+    LoanModule, 
+    TypeOrmModule.forRoot(database), 
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRES_IN: Joi.string().default('30m')
+      })
+    }),
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
